b432d3a940f8e4caff71ad5365fdedfb7e8e76f9
Sarika.Sairam <sairamsarika24@gmail.com>
2023-03-16 15:58:48 +0530

    face remaked

--- src/Pages/LoginTypes/Face.jsx
+++ src/Pages/LoginTypes/Face.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import { PATHS } from '../../utils/constants';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { PATHS } from "../../utils/constants";
 const Face = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
     <div>
-      <h1>Face</h1>
-      <button onClick={()=>navigate(PATHS.dashboard)}>Dashboard</button>
+      <h1>This is Face Page</h1>
+      <button onClick={() => navigate(PATHS.dashboard)}>Dashboard</button>
     </div>
-  )
-}
+  );
+};
 
-export default Face
+export default Face;
